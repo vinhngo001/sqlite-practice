417cd7343d1653fc362eea30ecac570601798396
vinhngo1907 <vinhtrungngo1907@gmail.com>
2023-07-07 20:00:20 +0700

    commit

--- routes/department.js
+++ routes/department.js
@@ -29,6 +29,42 @@ router.get("/", async (req, res) => {
             res.status(200).json({ success: "Lấy dữ liệu thành công", department: rows });
         }
     });
-})
+});
+
+router.put("/:id", async (req, res) => {
+    const { code, name } = req.body;
+    const { id } = req.params;
+    let sql = `UPDATE Department SET code=?, name=? WHERE id=?`;
+
+    let inputData = [];
+    if (name) inputData.push(name);
+    if (code) inputData.push(code);
+
+    db.run(sql, [...inputData, id], function (err) {
+        if (err) {
+            return res.status(400).json({ error: err.message });
+        }
+
+        res.json({ success: "Cập nhật dữ liệu thành công" });
+    })
+});
+
+router.delete("/:id", async (req, res) => {
+    let sql = `DELETE FROM Department WHERE id = ?`;
+    const { id } = req.params;
+    db.get(`SELECT * FROM Department_Employee WHERE departmentId= ?`, [id], function (err, row) {
+        if (row) {
+            return res.status(400).json({ error: "Phòng ban đang có nhân viên làm việc" });
+        } else {
+            db.run(sql, [id], function (err) {
+                if (err) {
+                    return res.status(400).json({ error: err.message });
+                }
+
+                res.json({ success: "Xóa dữ liệu thành công" });
+            });
+        }
+    })
+});
 
 module.exports = router;
